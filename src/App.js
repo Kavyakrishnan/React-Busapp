9282e09236db5c11f2673774b7f69e535dc733d5
Kavyakrishnan <kavyakrishnan9821@gmail.com>
2022-05-24 16:35:36 +0530

    complete

--- src/App.js
+++ src/App.js
@@ -4,12 +4,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import Addbus from './Component/Addbus';
 import Searchbus from './Component/Searchbus';
+import Header from './Component/Header';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
     <div >
-      <Addbus/>
-      <Searchbus/>
+      <BrowserRouter>
+      <Routes>
+        <Route path="/"exact element={<Addbus/>}/>
+        <Route path="/search"exact element={<Searchbus/>}/>
+
+      </Routes>
+      </BrowserRouter>
+     
     </div>
   );
 }
--- src/Component/Searchbus.js
+++ src/Component/Searchbus.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Header from './Header'
+
 
 const Searchbus = () => {
   return (
-    <div><div className="Container">
+    <div>
+        <Header/>
+        <div className="Container">
     <div className="row g-2">
         <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
             <div className="row g-3">
